8662ec824fa333068cb33616ce407b7fe77cde1a
irrako <irraquex@gmail.com>
2020-06-04 22:14:36 -0400

    postman

--- routes/service.js
+++ routes/service.js
@@ -6,6 +6,7 @@ res.status(200).json({msn: "Hola mundo"});
 });
 
 router.post('/test', function(req, res, next) {
+    req.body["msn"] = "Por el servidor";
     var data = req.body
     res.status(200).json(data);
     });
